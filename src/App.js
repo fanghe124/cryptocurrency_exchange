44217d06c9c6feb6e07f71548eee7b6b948ee5cd
Kevin Pak <kedpak@yahoo.com>
2017-12-07 23:56:09 -0800

    add api request to grab data and display name of currencies in column

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import Header from './header';
 import Chart from './chart';
+import axios from 'axios';
 
 import './index.css';
 
 class App extends Component {
+
+  state = {
+    data: []
+  }
+
+
+  componentDidMount = () => {
+    axios.get('https://api.coinmarketcap.com/v1/ticker/')
+      .then(res => {
+        let data = res.data.map(obj => obj);
+        this.setState({data});
+      });
+  }
   render() {
     return (
       <div className="App">
         <Header />
-        <Chart />
+        <Chart data={this.state}/>
       </div>
     );
   }
--- src/chart.js
+++ src/chart.js
@@ -5,11 +5,16 @@ import Chartheader from './chartHeader';
 class Chart extends Component {
 
 
-
   render() {
+    const {data} = this.props;
+    console.log(data);
     return (
+
       <div className="chart">
         <Chartheader />
+        {data.data.map(cur => {
+          return <div className="currName">{cur.name}</div>
+        })}
       </div>
     );
   }
